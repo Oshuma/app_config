0f23a735c4fae8e70d406c62bb6373e3e191d282
Dale Campbell <dale@save-state.net>
2009-03-10 20:12:39 -0500

    config shit

--- lib/api_store.rb
+++ lib/api_store.rb
@@ -1,7 +1,30 @@
-module ApiStore
+class ApiStore
   VERSION = '0.0.1'
 
   def self.to_version
     "#{self.class} v#{VERSION}"
   end
+
+  attr_accessor :storage_method
+
+  def initialize(opts = {}, &block)
+    opts.each_pair { |key, value| self.send("#{key}=", value) }
+    yield self if block_given?
+    @storage = nil
+    initialize_storage
+  end
+
+  def self.configure(opts = {}, &block)
+    new(opts, &block)
+  end
+
+private
+
+  def initialize_storage
+    case storage_method
+    when :yaml
+      # TODO: yaml storage
+    end
+  end
+
 end
--- spec/api_store_spec.rb
+++ spec/api_store_spec.rb
@@ -1,7 +1,23 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
 describe ApiStore do
+  before(:each) do
+    @valid_options = {
+      :storage_method => :yaml,
+    }
+  end
+
   it 'should have a version' do
     ApiStore.to_version.should_not be_nil
+  end
+
+  it 'should raise error on unknown option' do
+    lambda do
+      ApiStore.configure(@valid_options.merge(:unknown => 'option'))
+    end.should raise_error(NoMethodError)
+  end
+
+  it 'should accept a config block' do
+    ApiStore.configure {|c| c}.should_not be_nil
   end
 end
