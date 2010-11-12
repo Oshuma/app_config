945297d77d3d526815b633c0610bb94a41e8d8fc
Dale Campbell <oshuma@gmail.com>
2010-11-12 17:02:00 -0500

    i guess it's a good thing this was never thrown, since it didn't exist before now. lawl.

--- lib/app_config/error.rb
+++ lib/app_config/error.rb
@@ -1,6 +1,12 @@
 module AppConfig
   module Error
 
+    class MustOverride < Exception
+      def initialize(method)
+        super("Must override method: #{method}")
+      end
+    end
+
     class NotSetup < Exception
       def to_s; "Must call 'AppConfig.setup' to setup storage!"; end
     end
