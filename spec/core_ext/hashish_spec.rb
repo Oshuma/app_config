5c1ad4324bee3d4fedf488adffaf3d0bd81e0c3e
DSIW <dsiw@dsiw-it.de>
2012-09-15 14:46:15 +0200

    Add specs to check convert methods and setter

--- spec/core_ext/hashish_spec.rb
+++ spec/core_ext/hashish_spec.rb
@@ -6,6 +6,47 @@ describe Hashish do
     @symbols = { :key  => 'value', :four  => 20 }
   end
 
+  describe "Setter" do
+    subject { Hashish.new }
+
+    it 'should set new pairs by store()' do
+      subject.store(:key, 'value')
+      subject[:key].should == 'value'
+    end
+
+    it 'should set new pairs by []=' do
+      subject[:key] = 'value'
+      subject[:key].should == 'value'
+    end
+
+    it 'should set new pairs by dynamic method' do
+      subject.key = 'value'
+      subject[:key].should == 'value'
+    end
+
+    it 'should set new pairs by dynamic method with prefix' do
+      subject._key = 'value'
+      subject[:key].should == 'value'
+    end
+  end
+
+  describe "Convert to other formats" do
+    subject { Hashish.new(@symbols) }
+
+    it "should convert to JSON" do
+      subject.to_json.should eq "{\"key\":\"value\",\"four\":20}"
+    end
+
+    it "should convert to YAML" do
+      subject.to_yaml.should eq "---\nkey: value\nfour: 20\n"
+    end
+
+    it "should convert to Hash" do
+      hash = {"key" => "value", "four" => 20}
+      subject.to_hash.should eq hash
+    end
+  end
+
   it 'should not give a fuck about symbols' do
     hashish = Hashish.new(@strings)
     hashish[:key].should == 'value'
